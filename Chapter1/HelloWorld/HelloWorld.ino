1628b47a742beadd89d165ee4b1c1378d691cdcc
dhaberst <daniel.haberstock1@gmail.com>
2018-05-04 17:07:28 -0600

    Started Chapter1 with a new program Hello World! :cat:.

---
+++ Chapter1/HelloWorld/HelloWorld.ino
@@ -0,0 +1,12 @@
+/*
+  Hello World
+
+  A simple program that print 'Hello World!' to the console
+*/
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Hello world!");
+}
+
+void loop() {
+}
